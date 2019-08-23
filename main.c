2f98841c25064ca9cf5537bbe875432bdf817f4b
James <James@voxdsp.com>
2019-08-23 14:31:04 +0100

    general

--- main.c
+++ main.c
@@ -3586,7 +3586,7 @@ int main(int argc , char *argv[])
                     //priv as bytes
                     struct addr subg_priv;
                     size_t len = ECC_CURVE;
-                    b58tobin(subg_priv.key, &len, bpriv, strlen(bpriv)-1);
+                    b58tobin(subg_priv.key, &len, bpriv, strlen(bpriv));
 
                     //Gen Public Key
                     struct addr subg_pub;
