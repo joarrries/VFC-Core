8a37e07328e764f80e1d5c6ebacbfda96f1819f3
James <James@voxdsp.com>
2019-08-23 14:21:30 +0100

    general

--- main.c
+++ main.c
@@ -3538,6 +3538,8 @@ int main(int argc , char *argv[])
                     //base58 priv key
                     char* bpriv = strtok(l, " ");
 
+                    printf("A: %sf\n", bpriv);
+
                     //priv as bytes
                     struct addr subg_priv;
                     size_t len = ECC_CURVE+1;
@@ -3550,7 +3552,7 @@ int main(int argc , char *argv[])
                     //Get balance of pub key
                     const double bal = toDB(getBalanceLocal(&subg_pub));
 
-                    printf("B: %.3f\n", bal);
+                    printf("C: %.3f\n", bal);
 
                     //Print private key & balance 
                     if(bal > 0)
